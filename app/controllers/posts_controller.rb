42209babb32cab4724d9cfa8acf65dc423042da3
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-11-21 21:34:50 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index # indexアクションを定義
+    # @post = Post.find(1) # 1番目のレコードを@postに代入
+    @posts = Post.all
+  end
+  
+  def new
+
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
